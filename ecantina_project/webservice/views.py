5b91ab2f3b8c317834f693c0ecd16239aa309d06
Bartlomiej Mika <bmika@icloud.com>
2015-07-16 21:55:03 -0400

    Submitting function interface prototyping.

--- ecantina_project/webservice/views.py
+++ ecantina_project/webservice/views.py
@@ -64,10 +64,61 @@ def process_request(jsonrpc, id, method, params):
 
 
 def hello_world(jsonrpc, id, method, params):
+    """
+        Test function used by developers to test out making JSON-RPC calls
+        for learning and testing purposes which won't break anything.
+    """
     return {'jsonrpc': jsonrpc, 'id': id, 'result': 'Hello World!', }
 
 
 def add(jsonrpc, id, method, params):
-    a = float(params.get("a"))
-    b = float(params.get("b"))
+    """
+        Test function used by developers to test out making JSON-RPC calls
+        involving parameters.
+    """
+    try:
+        a = float(params.get("a"))
+    except:
+        a = 0
+    try:
+        b = float(params.get("b"))
+    except:
+        b = 0
     return {'jsonrpc': jsonrpc, 'id': id, 'result': a+b, }
+
+
+def open_cart(jsonrpc, id, method, params):
+    """
+        Opens a new cart to be loaded in by the store employee. If a previous
+        cart existed it will be deleted.
+    """
+    return {'jsonrpc': jsonrpc, 'id': id, 'result': 'todo', }
+
+
+def assign_cart(jsonrpc, id, method, params):
+    """
+        Assign a customer to the current cart.
+    """
+    return {'jsonrpc': jsonrpc, 'id': id, 'result': 'todo', }
+
+
+def add_to_cart(jsonrpc, id, method, params):
+    """
+        Assign a product to the current cart.
+    """
+    return {'jsonrpc': jsonrpc, 'id': id, 'result': 'todo', }
+
+
+def checkout_cart(jsonrpc, id, method, params):
+    """
+        Close the cart and tell the system to handle dealing with a
+        finished purchasing session.
+    """
+    return {'jsonrpc': jsonrpc, 'id': id, 'result': 'todo', }
+
+
+def add_to_checkedout_cart(jsonrpc, id, method, params):
+    """
+        Assign a product to the cart which was previously checked out.
+    """
+    return {'jsonrpc': jsonrpc, 'id': id, 'result': 'todo', }
