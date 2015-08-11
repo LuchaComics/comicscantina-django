000a3c0a2dbbe9a48607f97da7c2949004585031
Bartlomiej Mika <bmika@icloud.com>
2015-08-11 17:23:26 -0400

    Latest progress with adding receipt details.

--- ecantina_project/inventory_order/urls.py
+++ ecantina_project/inventory_order/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = patterns('',
-    url(r'^inventory/(\d+)/(\d+)/orders$', views.orders_record_page),
+    url(r'^inventory/(\d+)/(\d+)/orders$', views.orders_page),
+    url(r'^inventory/(\d+)/(\d+)/orders/(\d+)/$', views.order_details_page),
 )
--- ecantina_project/inventory_order/views.py
+++ ecantina_project/inventory_order/views.py
@@ -1,4 +1,5 @@
 import json
+from decimal import *
 from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
@@ -8,17 +9,41 @@ from django.contrib.auth.decorators import login_required
 from api.models.ec.organization import Organization
 from api.models.ec.employee import Employee
 from api.models.ec.store import Store
+from api.models.ec.receipt import Receipt
 
 
 @login_required(login_url='/inventory/login')
-def orders_record_page(request, org_id, store_id):
-    return render(request, 'inventory_order/index.html',{
-                  'org': Organization.objects.get(org_id=org_id),
-                  'store': Store.objects.get(store_id=store_id),
-                  'tab':'orders',
-                  'employee': Employee.objects.get(user=request.user),
-                  'locations': Store.objects.filter(organization_id=org_id),
-                  'local_css_library':settings.INVENTORY_CSS_LIBRARY,
-                  'local_js_library_header':settings.INVENTORY_JS_LIBRARY_HEADER,
-                  'local_js_library_body':settings.INVENTORY_JS_LIBRARY_BODY,
-                  })
+def orders_page(request, org_id, store_id):
+    return render(request, 'inventory_order/master.html',{
+        'org': Organization.objects.get(org_id=org_id),
+        'store': Store.objects.get(store_id=store_id),
+        'tab':'orders',
+        'employee': Employee.objects.get(user=request.user),
+        'locations': Store.objects.filter(organization_id=org_id),
+        'local_css_library':settings.INVENTORY_CSS_LIBRARY,
+        'local_js_library_header':settings.INVENTORY_JS_LIBRARY_HEADER,
+        'local_js_library_body':settings.INVENTORY_JS_LIBRARY_BODY,
+    })
+
+
+@login_required(login_url='/inventory/login')
+def order_details_page(request, org_id, store_id, receipt_id):
+    try:
+        receipt = Receipt.objects.get(receipt_id=receipt_id)
+    except Receipt.DoesNotExist:
+        receipt = None
+    store = Store.objects.get(store_id=store_id)
+    tax_rate = Decimal(store.tax_rate) * Decimal(100)
+    return render(request, 'inventory_order/detail.html',{
+        'org': Organization.objects.get(org_id=org_id),
+        'store': store,
+        'receipt': receipt,
+        'tax_rate': tax_rate,
+        'tab':'orders',
+        'employee': Employee.objects.get(user=request.user),
+        'locations': Store.objects.filter(organization_id=org_id),
+        'local_css_library':settings.INVENTORY_CSS_LIBRARY,
+        'local_js_library_header':settings.INVENTORY_JS_LIBRARY_HEADER,
+        'local_js_library_body':settings.INVENTORY_JS_LIBRARY_BODY,
+    })
+
