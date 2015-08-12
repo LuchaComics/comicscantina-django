39df429c614b1f3810a35de57ece3ed0cd2d8552
Bartlomiej Mika <bmika@icloud.com>
2015-08-12 13:44:07 -0400

    Added unit tests for help requests.

--- ecantina_project/inventory_help/tests.py
+++ ecantina_project/inventory_help/tests.py
@@ -1,3 +1,64 @@
+import json
+from datetime import datetime
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.http import QueryDict
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.conf.urls.static import static, settings
+from captcha.models import CaptchaStore
+from . import views
+from api.models.ec.imageupload import ImageUpload
+from api.models.ec.organization import Organization
+from api.models.ec.store import Store
+from api.models.ec.employee import Employee
+from api.models.ec.section import Section
+from inventory.tests.sample import SamplDataPopulator
 
-# Create your tests here.
+
+# Contants
+TEST_USER_EMAIL = "ledo@gah.com"
+TEST_USER_USERNAME = TEST_USER_EMAIL
+TEST_USER_PASSWORD = "password"
+
+# Extra parameters to make this a Ajax style request.
+KWARGS = {'HTTP_X_REQUESTED_WITH':'XMLHttpRequest'}
+
+
+class HelpTestCase(TestCase):
+    """
+        Run in Console:
+        python manage.py test inventory_help.tests
+    """
+    def tearDown(self):
+        # Clear Sample Data
+        populator = SamplDataPopulator()
+        populator.dealloc()
+    
+    def setUp(self):
+        captcha_count = CaptchaStore.objects.count()
+        self.failUnlessEqual(captcha_count, 0)
+        now = datetime.now()
+        
+        # Create Sample Data
+        populator = SamplDataPopulator()
+        populator.populate()
+    
+    def test_url_resolves_to_help_page(self):
+        found = resolve('/inventory/1/1/help')
+        self.assertEqual(found.func, views.help_page)
+
+    def test_help_page_returns_correct_html(self):
+        client = Client()
+        client.login(
+            username=TEST_USER_USERNAME,
+            password=TEST_USER_PASSWORD
+        )
+        response = client.post('/inventory/1/1/help')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b' Contact Us',response.content)
+        self.assertIn(b'id_hidden_upload_id',response.content)
+
