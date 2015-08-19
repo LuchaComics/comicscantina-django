d2772b4c9ad17368055c62e1e5fa05d2e760a06f
Bartlomiej Mika <bmika@icloud.com>
2015-08-19 18:07:14 -0400

    Latest progress with pullist.

--- ecantina_project/inventory_pulllist/urls.py
+++ ecantina_project/inventory_pulllist/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^inventory/(\d+)/(\d+)/pulllist$', views.pulllist_page),
+    url(r'^inventory/(\d+)/(\d+)/pulllist/(\d+)/subscriptions$', views.pulllist_subscriptions_page),
 )
--- ecantina_project/inventory_pulllist/views.py
+++ ecantina_project/inventory_pulllist/views.py
@@ -9,13 +9,40 @@ from api.models.ec.imageupload import ImageUpload
 from api.models.ec.organization import Organization
 from api.models.ec.employee import Employee
 from api.models.ec.store import Store
+from api.models.ec.pulllist import Pulllist
+from api.models.ec.pulllistsubscription import PulllistSubscription
 
 
 @login_required(login_url='/inventory/login')
 def pulllist_page(request, org_id, store_id):
+    try:
+        pulllists = Pulllist.objects.filter(organization_id=org_id)
+    except Pulllist.DoesNotExist:
+        pulllists = None
     return render(request, 'inventory_pulllist/view.html',{
         'org': Organization.objects.get(org_id=org_id),
         'store': Store.objects.get(store_id=store_id),
+        'pulllists': pulllists,
+        'tab':'pulllist',
+        'employee': Employee.objects.get(user=request.user),
+        'locations': Store.objects.filter(organization_id=org_id),
+        'local_css_library':settings.INVENTORY_CSS_LIBRARY,
+        'local_js_library_header':settings.INVENTORY_JS_LIBRARY_HEADER,
+        'local_js_library_body':settings.INVENTORY_JS_LIBRARY_BODY,
+    })
+
+@login_required()
+def pulllist_subscriptions_page(request, org_id, store_id, pulllist_id):
+    pulllist = Pulllist.objects.get(pulllist_id=pulllist_id)
+    try:
+        subscriptions = PulllistSubscription.objects.filter(pulllist_id=pulllist_id)
+    except PulllistSubscription.DoesNotExist:
+        subscriptions = None
+    return render(request, 'inventory_pulllist/rows.html',{
+        'org': Organization.objects.get(org_id=org_id),
+        'store': Store.objects.get(store_id=store_id),
+        'pulllist': pulllist,
+        'subscriptions': subscriptions,
         'tab':'pulllist',
         'employee': Employee.objects.get(user=request.user),
         'locations': Store.objects.filter(organization_id=org_id),
