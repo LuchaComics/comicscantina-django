1c2500e0cb8fb0f2a2ae4ab6ee3db1e0efa39303
Bartlomiej Mika <bmika@icloud.com>
2015-08-20 20:22:59 -0400

    Added pagination to pulllist subscriptions.

--- ecantina_project/api/views/pulllistsubscription.py
+++ ecantina_project/api/views/pulllistsubscription.py
@@ -1,11 +1,22 @@
+import django_filters
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import filters
+from api.pagination import LargeResultsSetPagination
 from api.permissions import BelongsToCustomerOrIsEmployeeUser
 from api.models.ec.pulllistsubscription import PulllistSubscription
 from api.serializers import PulllistSubscriptionSerializer
+
+
+class PulllistSubscriptionFilter(django_filters.FilterSet):
+    customer = django_filters.CharFilter(name="customer__customer_id")
+    organization = django_filters.CharFilter(name="organization__org_id")
+    pulllist = django_filters.CharFilter(name="pulllist__pulllist_id")
+    class Meta:
+        model = PulllistSubscription
+        fields = ['customer','organization', 'pulllist', ]
 
 
 class PulllistSubscriptionViewSet(viewsets.ModelViewSet):
@@ -14,7 +25,8 @@ class PulllistSubscriptionViewSet(viewsets.ModelViewSet):
     """
     queryset = PulllistSubscription.objects.all()
     serializer_class = PulllistSubscriptionSerializer
+    pagination_class = LargeResultsSetPagination
     permission_classes = (BelongsToCustomerOrIsEmployeeUser, IsAuthenticated)
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('customer__customer_id', 'pulllist__pulllist_id', 'organization__org_id',)
+    filter_class = PulllistSubscriptionFilter
 
