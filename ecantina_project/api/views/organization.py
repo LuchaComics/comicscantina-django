fe7a60f903b6bd8649106c285b0442054c6f6efb
Bartlomiej Mika <bmika@icloud.com>
2015-08-20 21:09:55 -0400

    Added more pagination to app: organizations and stores.

--- ecantina_project/api/views/organization.py
+++ ecantina_project/api/views/organization.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import filters
+from api.pagination import LargeResultsSetPagination
 from api.permissions import BelongsToOrganizationOrReadOnly
 from api.models.ec.organization import Organization
 from api.models.ec.employee import Employee
@@ -15,6 +16,7 @@ class OrganizationViewSet(viewsets.ModelViewSet):
     """
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+    pagination_class = LargeResultsSetPagination
     permission_classes = (BelongsToOrganizationOrReadOnly, IsAuthenticatedOrReadOnly)
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = ('org_id',)
