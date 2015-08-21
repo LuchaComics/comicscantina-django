4248a9c123e7272973eadd13dbb2b91f027a76b4
Bartlomiej Mika <bmika@icloud.com>
2015-08-20 21:14:15 -0400

    Added pagination fixes to store settings.

--- ecantina_project/api/views/section.py
+++ ecantina_project/api/views/section.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import filters
+from api.pagination import LargeResultsSetPagination
 from api.permissions import BelongsToOrganization
 from api.serializers import SectionSerializer
 from api.models.ec.section import Section
@@ -14,6 +15,7 @@ class SectionViewSet(viewsets.ModelViewSet):
     """
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
+    pagination_class = LargeResultsSetPagination
     permission_classes = (BelongsToOrganization, IsAuthenticated)
     filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = ('organization', 'store',)
