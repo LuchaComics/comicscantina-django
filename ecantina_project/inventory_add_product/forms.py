23fe4ebe4d41222641091359dc7b64a519741512
Bartlomiej Mika <bmika@icloud.com>
2015-08-25 17:55:54 -0400

    Latest progress with developing product adding.

--- ecantina_project/inventory_add_product/forms.py
+++ ecantina_project/inventory_add_product/forms.py
@@ -32,16 +32,8 @@ class ComicForm(forms.ModelForm):
         }
 
 
-from datetime import date
-from django.db import models
-from django import forms
-from django.forms import ModelForm, Textarea, TextInput, NumberInput
-from django.forms.extras.widgets import Select, SelectDateWidget
-from django.forms.widgets import EmailInput
-from django.conf import settings
-from django.core.exceptions import ValidationError
-from api.models.gcd.issue import GCDIssue
-from api.models.ec.comic import Comic
+
+
 
 
 class IssueForm(forms.Form):
