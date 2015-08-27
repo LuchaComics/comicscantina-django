052fd113264ca6c37ae27a8055fa473e8219e38f
Bartlomiej Mika <bmika@icloud.com>
2015-08-27 14:08:13 -0400

    Change back to image_id.

--- ecantina_project/api/models/ec/product.py
+++ ecantina_project/api/models/ec/product.py
@@ -98,7 +98,7 @@ class Product(models.Model):
     )
                               
     # Every product has images.
-    image = models.ImageField(upload_to='upload', null=True, blank=True)
+    image = models.ForeignKey(ImageUpload, null=True, blank=True, on_delete=models.SET_NULL)
     images = models.ManyToManyField(ImageUpload, blank=True, related_name='product_images')
     
     # Products need to belong to a specific organization and where it is located
