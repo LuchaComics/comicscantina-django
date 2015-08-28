045370c499b914767c1be31452067d903389ffaf
Bartlomiej Mika <bmika@icloud.com>
2015-08-28 15:13:33 -0400

    Minor refactoring done.

--- ecantina_project/inventory_checkout/views/pos_session.py
+++ ecantina_project/inventory_checkout/views/pos_session.py
@@ -1,7 +1,4 @@
-import json
 from django.shortcuts import render
-from django.core import serializers
-from django.http import HttpResponse
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -12,7 +9,7 @@ from api.models.ec.store import Store
 
 @login_required(login_url='/inventory/login')
 def checkout_page(request, org_id, store_id):
-    return render(request, 'inventory_checkout/session/index.html',{
+    return render(request, 'inventory_checkout/session/view.html',{
         'org': Organization.objects.get(org_id=org_id),
         'store': Store.objects.get(store_id=store_id),
         'tab':'checkout',
