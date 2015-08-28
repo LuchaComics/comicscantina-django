b358b0b0642a3a1b8871f202e7f232749a8df6f1
Bartlomiej Mika <bmika@icloud.com>
2015-08-28 14:07:53 -0400

    Added placeholder code for print_labels.

--- ecantina_project/inventory_print_label/urls.py
+++ ecantina_project/inventory_print_label/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
+from . import views
 
 
 urlpatterns = patterns('',
-
+    url(r'^inventory/(\d+)/(\d+)/print_labels/comics$', views.print_labels_page),
 )
