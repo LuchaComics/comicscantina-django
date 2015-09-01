0503106b3f8fef53b17b3bdc00e99037ba21e43a
Chad Smith <chad.j.smith@gmail.com>
2015-09-01 16:28:14 -0400

    Products page changes...will have to figure out a better way to list products of different types...

--- ecantina_project/store_products/views/product_list.py
+++ ecantina_project/store_products/views/product_list.py
@@ -8,14 +8,21 @@ from api.models.ec.category import Category
 from api.models.ec.organization import Organization
 from api.models.ec.tag import Tag
 from api.models.ec.promotion import Promotion
+from api.models.ec.product import Product
 
 
-def list_page(request, org_id=0):
+def list_page(request, org_id=0, cat_id=0):
     organization = Organization.objects.get(org_id=org_id)
-    categories = Category.objects.all()
+    categories = Category.objects.all().order_by('category_id')
+    try:
+        current_category = Category.objects.get(category_id=cat_id)
+    except:
+        current_category = 'All Products'
     brands = Brand.objects.all()
     tags = Tag.objects.all()
     promotions = Promotion.objects.all()
+
+    products = Product.objects.filter(organization=org_id, category=cat_id)
 
 
     return render(request, 'store_products/product_list/list.html',{
@@ -24,7 +31,9 @@ def list_page(request, org_id=0):
         'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
         'organization' :organization,
         'categories' : categories,
+        'current_category' : current_category,
         'brands' : brands,
         'tags' : tags,
         'promotions' : promotions,
+        'products' : products,
     })
