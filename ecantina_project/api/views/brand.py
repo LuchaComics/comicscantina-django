524fbe3ba95f1271088670bc4a35d3e5cf42a5e5
Bartlomiej Mika <bmika@icloud.com>
2015-09-08 16:43:40 -0400

    Latest beta copy of Quick Add.

--- ecantina_project/api/views/brand.py
+++ ecantina_project/api/views/brand.py
@@ -1,3 +1,4 @@
+import django_filters
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -13,8 +14,16 @@ from api.models.ec.employee import Employee
 from api.models.ec.promotion import Promotion
 
 
+class BrandFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(name="name", lookup_type=("icontains"))
+    class Meta:
+        model = Brand
+        fields = ['brand_id', 'name',]
+
+
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
     pagination_class = LargeResultsSetPagination
     permission_classes = (IsAdminUserOrReadOnly, IsAuthenticatedOrReadOnly,)
+    filter_class = BrandFilter
