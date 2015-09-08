97b294486cba319ea943a9ddc5d9066792230623
chad <chad.j.smith@gmail.com>
2015-09-08 16:16:50 -0400

    Product page changes, need to figure out how best to display products...creating some test products using images / comics from gcd

--- ecantina_project/store_products/urls.py
+++ ecantina_project/store_products/urls.py
@@ -4,6 +4,8 @@ from store_products.views import product_list
 urlpatterns = patterns('',
     url(r'^products/list$', product_list.list_page),
     url(r'^products/list/$', product_list.list_page),
+    url(r'^products/list/0$', product_list.list_page),
+    url(r'^products/list/0/$', product_list.list_page),
     url(r'^products/list/(\d+)$', product_list.list_page),
     url(r'^products/list/(\d+)/$', product_list.list_page),
     url(r'^(\d+)/products/list$', product_list.list_page),
--- ecantina_project/store_products/views/product_list.py
+++ ecantina_project/store_products/views/product_list.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from api.models.ec.brand import Brand
 from api.models.ec.category import Category
 from api.models.ec.organization import Organization
@@ -14,16 +15,33 @@ from api.models.ec.product import Product
 def list_page(request, org_id=0, cat_id=0):
     organization = Organization.objects.get(org_id=org_id)
     categories = Category.objects.all().order_by('category_id')
-    try:
-        current_category = Category.objects.get(category_id=cat_id)
-    except:
-        current_category = 'All Products'
+    current_category = Category.objects.get(category_id=cat_id)
     brands = Brand.objects.all()
     tags = Tag.objects.all()
     promotions = Promotion.objects.all()
 
-    products = Product.objects.filter(organization=org_id, category=cat_id)
+    if current_category == 'All Products':
+        products = Product.objects.filter(organization=org_id)
+        prod_count = Product.objects.filter(organization=org_id)
+    elif current_category.parent_id == 0:
+        products = Product.objects.filter(organization=org_id, type=cat_id)
+        prod_count = Product.objects.filter(organization=org_id, type=cat_id)
+    else:
+        products = Product.objects.filter(organization=org_id, category=cat_id)
+        prod_count = Product.objects.filter(organization=org_id, category=cat_id)
 
+
+    parent_category = current_category.parent_id
+
+    paginator = Paginator(products, 3)
+
+    page = request.GET.get('page')
+    try:
+        prod_page = paginator.page(page)
+    except PageNotAnInteger:
+        prod_page = paginator.page(1)
+    except EmptyPage:
+        prod_page = paginator.page(paginator.num_pages)
 
     return render(request, 'store_products/product_list/list.html',{
         'local_css_library' : settings.STORE_CSS_LIBRARY,
@@ -35,5 +53,9 @@ def list_page(request, org_id=0, cat_id=0):
         'brands' : brands,
         'tags' : tags,
         'promotions' : promotions,
-        'products' : products,
+        'products' : prod_page,
+        'product_count' : prod_count,
+
     })
+
+
