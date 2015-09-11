ab7c1eee99e9290cd1aafe8630eb2d4361703779
Bartlomiej Mika <bmika@icloud.com>
2015-09-11 12:00:10 -0400

    Prototype code of listing the QR Codes.

--- ecantina_project/inventory_print_label/views/comic.py
+++ ecantina_project/inventory_print_label/views/comic.py
@@ -23,7 +23,7 @@ def comics_print_labels_page(request, org_id, store_id):
     # Fetch all the comics starting with the most recent submission
     # grouped by comic series.
     q = Comic.objects.filter(product__store_id=store_id)
-    q = q.order_by('created')
+    q = q.order_by('issue__series')
     q.query.group_by = ['issue__series']
 
     paginator = Paginator(q, 100) # Show 100 comics per page
