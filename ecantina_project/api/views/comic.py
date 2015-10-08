fb8b17afe4d885b750f2057cd8e87dd4707ecf7a
Bartlomiej Mika <bmika@icloud.com>
2015-10-08 19:52:58 -0400

    BUGFIX: For issue #157

--- ecantina_project/api/views/comic.py
+++ ecantina_project/api/views/comic.py
@@ -15,15 +15,16 @@ from api.models.ec.employee import Employee
 class ComicFilter(django_filters.FilterSet):
     organization = django_filters.CharFilter(name="organization__organization_id")
     product = django_filters.CharFilter(name="product__product_id")
-    issue = django_filters.CharFilter(name="issue__issue_id")
     series = django_filters.CharFilter(name="issue__series_id")
+    issue = django_filters.CharFilter(name="issue__issue_id")
+    is_sold = django_filters.BooleanFilter(name="product__is_sold")
     is_newsstand_edition = django_filters.BooleanFilter(name="is_newsstand_edition")
     is_retail_incentive_variant = django_filters.BooleanFilter(name="is_retail_incentive_variant")
     is_variant_cover = django_filters.BooleanFilter(name="is_variant_cover")
     is_canadian_priced_variant = django_filters.BooleanFilter(name="is_canadian_priced_variant")
     class Meta:
         model = Comic
-        fields = ['comic_id', 'is_cgc_rated', 'age', 'cgc_rating', 'label_colour', 'condition_rating', 'is_canadian_priced_variant', 'is_variant_cover', 'is_retail_incentive_variant', 'is_newsstand_edition', 'issue', 'series', 'product', 'created', 'organization',]
+        fields = ['comic_id', 'is_cgc_rated', 'age', 'cgc_rating', 'label_colour', 'condition_rating', 'is_canadian_priced_variant', 'is_variant_cover', 'is_retail_incentive_variant', 'is_newsstand_edition', 'issue', 'series', 'product', 'created', 'organization', 'is_sold']
 
 
 class ComicViewSet(viewsets.ModelViewSet):
