bd7d12b0544ad12b3843962164cb51b3f95a13b4
Bartlomiej Mika <bmika@icloud.com>
2015-10-08 13:28:59 -0400

    Updated support new catalog. #153

--- ecantina_project/etl/management/commands/gcd_issue_cover_catalog_importer.py
+++ ecantina_project/etl/management/commands/gcd_issue_cover_catalog_importer.py
@@ -1,9 +1,9 @@
 import os
 import sys
-import xml.sax
+import xml.etree.ElementTree as ET
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from api.models.gcd.series import GCDSeries
+from api.models.gcd.issue import GCDIssue
 
 
 LARGE_ZOOM = 4
@@ -13,48 +13,61 @@ PRIMARY_IMAGE = 1
 ALTERNATIVE_IMAGE = 2
 
 
-class GCDSeriesCoverCatalogImporter(xml.sax.ContentHandler):
-    def __init__(self):
-        xml.sax.ContentHandler.__init__(self)
-        self.found_series = False
+class GCDIssueCoverCatalogImporter:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def begin_import(self):
+        for event, elem in ET.iterparse(self.file_path):
+            if elem.tag == "row":
+                self.import_row(elem)
+                elem.clear()
 
-    def import_from_attrs(self, attrs):
-        id = int(attrs.getValue("id"))
-        url = attrs.getValue("url")
-        print("GCDSeriesCoverCatalogImporter: Updating: " + str(id))
+    def import_row(self, row):
+        id = int(row.findtext('id'))
+        image_type = int(row.findtext('type'))
+        zoom = int(row.findtext('zoom'))
+        url = row.findtext('url')
+
+        print("GCDIssueCoverCatalogImporter: Updating: " + str(id))
         try:
-            series = Series.objects.get(series_id=id)
-        except Series.DoesNotExist:
-            print("Error: Series "+str(id)+" does not exist.")
+            issue = GCDIssue.objects.get(issue_id=id)
+        except GCDIssue.DoesNotExist:
+            print("Error: Issue "+str(id)+" does not exist.")
             return
-        series.cover_url = url
-        series.save()
 
-    def startElement(self, name, attrs):
-        if name == 'series':
-            self.import_from_attrs(attrs)
-
-    def characters(self, content):
-        pass
-
-    def endElement(self, name):
-        pass
+        if image_type is PRIMARY_IMAGE:
+            if zoom is SMALL_ZOOM:
+                issue.small_url = url
+            if zoom is MEDIUM_ZOOM:
+                issue.medium_url = url
+            if zoom is LARGE_ZOOM:
+                issue.large_url = url
+        elif image_type is ALTERNATIVE_IMAGE:
+            issue.has_alternative = True
+            if zoom is SMALL_ZOOM:
+                issue.alt_small_url = url
+            if zoom is MEDIUM_ZOOM:
+                issue.alt_medium_url = url
+            if zoom is LARGE_ZOOM:
+                issue.alt_large_url = url
+        else:
+            print("Error: Unknown Image Type: " + str(image_type))
+            return
+        issue.save()
 
 class Command(BaseCommand):
     """
-        ----------------------
-        import_gcd_covers
-        ----------------------
-        ETL will load up the catalog.xml file for the Series Cover assets and
-        update the associated Series in the database to reference the imported
-        covers.
-        
+        ---------------------------------
+        gcd_series_cover_catalog_importer
+        ---------------------------------
+       
         Run in your console:
-        $ python manage.py import_gcd_series_covers /Users/bartlomiejmika/Developer/comicscantina/py-comicscantina/scripts/cover/series_catalog.xml
+        $ python manage.py gcd_series_cover_catalog_importer /Users/bartlomiejmika/Developer/comicscantina/py-comicscantina/scripts/cover/issue_catalog.xml
         
         (Where that file path is the path to where the GCD XML files are located)
     """
-    help = 'ETL loads up the GCD database into our applicaton using the provided xml files.'
+    help = 'ETL updates our applicaton with the latest series images using the provided xml files.'
     
     def add_arguments(self, parser):
         parser.add_argument('file_path', nargs='+')
@@ -62,6 +75,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         os.system('clear;')  # Clear the console text.
         for file_path in options['file_path']:
-            print(file_path)
-            source = open(file_path)
-            xml.sax.parse(source, GCDSeriesCoverCatalogImporter())
+            importer = GCDIssueCoverCatalogImporter(full_file_path)
+            importer.begin_import()
