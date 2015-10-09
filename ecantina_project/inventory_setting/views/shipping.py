980311c272f528241f1e0172f60447dffb8af66b
Bartlomiej Mika <bmika@icloud.com>
2015-10-09 13:39:14 -0400

    Added shipping creation preference. #147

--- ecantina_project/inventory_setting/views/shipping.py
+++ ecantina_project/inventory_setting/views/shipping.py
@@ -7,17 +7,81 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from api.models.ec.imageupload import ImageUpload
 from api.models.ec.organization import Organization
 from api.models.ec.employee import Employee
 from api.models.ec.store import Store
-from api.models.ec.section import Section
-from api.models.ec.tag import Tag
+from api.models.ec.org_shipping_preference import OrgShippingPreference
+from api.models.ec.org_shipping_rates import OrgShippingRate
+from api.models.ec.store_shipping_preference import StoreShippingPreference
+from api.models.ec.store_shipping_rates import StoreShippingRate
 
 
 @login_required(login_url='/inventory/login')
 def shipping_settings_page(request, org_id, store_id):
+    # Get our organization preferences.
+    try:
+        org_preference = OrgShippingPreference.objects.get(organization_id=org_id)
+    except OrgShippingPreference.DoesNotExist:
+        org_preference = OrgShippingPreference.objects.create(
+            organization_id = org_id,
+        )
+
+    # If we don't have the shipping rates set then add them in now.
+    if len(org_preference.rates.all()) is 0:
+        # Create for Canda, United States and Mexico
+        canada = OrgShippingRate.objects.create(
+            organization_id = org_id,
+            country = 124,
+        )
+        org_preference.rates.add(canada)
+        united_states = OrgShippingRate.objects.create(
+            organization_id = org_id,
+            country = 840,
+        )
+        org_preference.rates.add(united_states)
+        mexico = OrgShippingRate.objects.create(
+            organization_id = org_id,
+            country = 484,
+        )
+        org_preference.rates.add(mexico)
+
+    # Get our store preferences.
+    try:
+        store_preference = StoreShippingPreference.objects.get(
+            organization_id=org_id,
+            store_id=store_id,
+        )
+    except StoreShippingPreference.DoesNotExist:
+        store_preference = StoreShippingPreference.objects.create(
+            organization_id = org_id,
+            store_id=store_id,
+        )
+    
+    # If we don't have the shipping rates set then add them in now.
+    if len(store_preference.rates.all()) is 0:
+        # Create for Canda, United States and Mexico
+        canada = StoreShippingRate.objects.create(
+            organization_id = org_id,
+            store_id=store_id,
+            country = 124,
+        )
+        store_preference.rates.add(canada)
+        united_states = StoreShippingRate.objects.create(
+            organization_id = org_id,
+            store_id=store_id,
+            country = 840,
+        )
+        store_preference.rates.add(united_states)
+        mexico = StoreShippingRate.objects.create(
+            store_id=store_id,
+            organization_id = org_id,
+            country = 484,
+        )
+        store_preference.rates.add(mexico)
+
     return render(request, 'inventory_setting/shipping/view.html',{
+        'org_preference': org_preference,
+        'store_preference': store_preference,
         'org': Organization.objects.get(org_id=org_id),
         'store': Store.objects.get(store_id=store_id),
         'employee': Employee.objects.get(user=request.user),
