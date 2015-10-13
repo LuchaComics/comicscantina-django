b29afa9d44f419a486943ef4d6de66168234accb
Bartlomiej Mika <bmika@icloud.com>
2015-10-13 16:11:17 -0400

    Added and replaced the brand images in store #166

--- ecantina_project/store_landpage/urls.py
+++ ecantina_project/store_landpage/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import patterns, include, url
-from store_landpage.views import aggregate
-from store_landpage.views import specific
+from store_landpage import views
 
 urlpatterns = patterns('',
     # Main Aggregate Store
-    url(r'^$', aggregate.front_page),
+    url(r'^$', views.aggregate_front_page),
     
     # Specific Store
-    url(r'^(\d+)$', specific.front_page),
-    url(r'^(\d+)/$', specific.front_page),
-    url(r'^(\d+)/store$', specific.front_page),
-    url(r'^(\d+)/landpage$', specific.front_page),
+#    url(r'^(\d+)$', specific.front_page),
+#    url(r'^(\d+)/$', specific.front_page),
+#    url(r'^(\d+)/store$', specific.front_page),
+#    url(r'^(\d+)/landpage$', specific.front_page),
 )
--- ecantina_project/store_landpage/views.py
+++ ecantina_project/store_landpage/views.py
@@ -4,9 +4,10 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.conf import settings
 from api.models.ec.comic import Comic
+from api.models.ec.brand import Brand
 
 
-def front_page(request, org_id=0):
+def aggregate_front_page(request):
     try:
         featured_comics = Comic.objects.filter(
             product__is_available=True,
@@ -27,10 +28,15 @@ def front_page(request, org_id=0):
     except Comic.DoesNotExist:
         new_comics = None
 
+    try:
+        brands = Brand.objects.all()
+    except Brand.objects.DoesNotExist:
+        brands = None
 
     return render(request, 'store_landpage/index.html',{
         'featured_comics': featured_comics,
         'new_comics': new_comics,
+        'brands': brands,
         'local_css_library' : settings.STORE_CSS_LIBRARY,
         'local_js_library_header' : settings.STORE_JS_LIBRARY_HEADER,
         'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
