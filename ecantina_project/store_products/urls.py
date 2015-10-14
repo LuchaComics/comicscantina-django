df3102ec27c5d186f0f6bbadc054baf2439b6fd1
Bartlomiej Mika <bmika@icloud.com>
2015-10-14 14:38:12 -0400

    Latest URL mapping to product details. #170

--- ecantina_project/store_products/urls.py
+++ ecantina_project/store_products/urls.py
@@ -3,12 +3,15 @@ from store_products.views import product_list
 from store_products.views import product_details
 
 urlpatterns = patterns('',
-    #product isting
+    # Product Listing
+    #----------------------
     url(r'^products/grid$', product_list.list_page),
     url(r'^(\d+)/products/grid$', product_list.list_page),
     url(r'^(\d+)/(\d+)/products/grid$', product_list.list_page),
-   #product details
-    url(r'^(\d+)/products/details/(\d+)$', product_details.details_page),
-    url(r'^(\d+)/products/details/(\d+)/$', product_details.details_page),
-
+                       
+    # Product Details
+    #----------------------
+    url(r'^products/(\d+)/$', product_details.details_page),
+    url(r'^(\d+)/products/(\d+)/$', product_details.details_page),
+    url(r'^(\d+)/(\d+)/products/(\d+)/$', product_details.details_page),
 )
--- ecantina_project/store_products/views/product_details.py
+++ ecantina_project/store_products/views/product_details.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 from django.conf import settings
 from api.models.ec.product import Product
 
-def details_page(request, org_id, prod_id):
-
-    product = Product.objects.get(organization=org_id, product_id=prod_id)
+def details_page(request, org_id=0, store_id=0, prod_id=0):
+    try:
+        product = Product.objects.get(organization=org_id, product_id=prod_id)
+    except Product.DoesNotExist:
+        product = None
 
     return render(request, 'store_products/product_details/details.html',{
         'local_css_library' : settings.STORE_CSS_LIBRARY,
