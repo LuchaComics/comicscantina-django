71589c6f32174043e815e097a73653c8b5e597b1
Bartlomiej Mika <bmika@icloud.com>
2015-10-14 10:10:11 -0400

    Latest change to enabling searching #169.

--- ecantina_project/store_products/urls.py
+++ ecantina_project/store_products/urls.py
@@ -4,14 +4,9 @@ from store_products.views import product_details
 
 urlpatterns = patterns('',
     #product isting
-    url(r'^products/list$', product_list.list_page),
-    url(r'^products/list/$', product_list.list_page),
-    url(r'^products/list/(\d+)$', product_list.list_page),
-    url(r'^products/list/(\d+)/$', product_list.list_page),
-    url(r'^(\d+)/products/list$', product_list.list_page),
-    url(r'^(\d+)/products/list/$', product_list.list_page),
-    url(r'^(\d+)/products/list/(\d+)$', product_list.list_page),
-    url(r'^(\d+)/products/list/(\d+)/$', product_list.list_page),
+    url(r'^products/grid$', product_list.list_page),
+    url(r'^(\d+)/products/grid$', product_list.list_page),
+    url(r'^(\d+)/(\d+)/products/grid$', product_list.list_page),
    #product details
     url(r'^(\d+)/products/details/(\d+)$', product_details.details_page),
     url(r'^(\d+)/products/details/(\d+)/$', product_details.details_page),
--- ecantina_project/store_products/views/product_list.py
+++ ecantina_project/store_products/views/product_list.py
@@ -7,35 +7,74 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from api.models.ec.brand import Brand
 from api.models.ec.category import Category
 from api.models.ec.organization import Organization
+from api.models.ec.store import Store
 from api.models.ec.tag import Tag
 from api.models.ec.promotion import Promotion
 from api.models.ec.product import Product
 
 
-def list_page(request, org_id=0, cat_id=0):
-    organization = Organization.objects.get(org_id=org_id)
-    categories = Category.objects.all().order_by('category_id')
-    brands = Brand.objects.all()
-    tags = Tag.objects.all()
-    promotions = Promotion.objects.all()
+def list_page(request, org_id=0, store_id=0):
+    org_id = int(org_id)
+    store_id = int(store_id)
+    
+    # Fetch the Organization / Store.
+    try:
+        organization = Organization.objects.get(org_id=org_id)
+    except Organization.DoesNotExist:
+        organization = None
+    try:
+        store = Store.objects.get(store_id=store_id)
+    except Store.DoesNotExist:
+        store = None
 
-    if cat_id == 0:
-        cat_id = 1
+    # Fetch objects used for searching criteria.
+    try:
+        categories = Category.objects.all().order_by('category_id')
+    except Category.DoesNotExist:
+        categories = None
 
+    try:
+        brands = Brand.objects.all()
+    except Brand.DoesNotExist:
+        brands = None
 
-    current_category = Category.objects.get(category_id=cat_id)
-    if current_category.parent_id == 0:
-        products = Product.objects.filter(organization=org_id, type=cat_id)
-        prod_count = Product.objects.filter(organization=org_id, type=cat_id)
-    else:
-        products = Product.objects.filter(organization=org_id, category=cat_id)
-        prod_count = Product.objects.filter(organization=org_id, category=cat_id)
+    try:
+        tags = Tag.objects.all()
+    except Tag.DoesNotExist:
+        tags = None
 
+    try:
+        promotions = Promotion.objects.all()
+    except Promotion.DoesNotExist:
+        promotion = None
 
-    parent_category = current_category.parent_id
+    # Get the categories and select the current category.
+    category_id = int(request.GET.get('category'))
+    try:
+        current_category = Category.objects.get(category_id=category_id)
+        
+        # Filter down the products based on the organization / store / category.
+        products = None
+        if current_category.parent_id is 0:
+            products = Product.objects.filter(category__parent_id=1)
+        else:
+            products = Product.objects.filter(category_id=category_id)
+        if organization:
+            products = products.filter(organization=organization)
+        if store:
+            products = products.filter(store=store)
+
+        prod_count = len(products)
+    except Category.DoesNotExist:
+        current_category = None
+        products = None
+        prod_count = 0
+    except Product.DoesNotExist:
+        products = None
+        prod_count = 0
+
 
     paginator = Paginator(products, 3)
-
     page = request.GET.get('page')
     try:
         prod_page = paginator.page(page)
@@ -45,18 +84,16 @@ def list_page(request, org_id=0, cat_id=0):
         prod_page = paginator.page(paginator.num_pages)
 
     return render(request, 'store_products/product_list/list.html',{
-        'local_css_library' : settings.STORE_CSS_LIBRARY,
-        'local_js_library_header' : settings.STORE_JS_LIBRARY_HEADER,
-        'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
-        'organization' :organization,
-        'categories' : categories,
-        'current_category' : current_category,
-        'brands' : brands,
-        'tags' : tags,
-        'promotions' : promotions,
-        'products' : prod_page,
-        'product_count' : prod_count,
-
+        'categories': categories,
+        'current_category': current_category,
+        'brands': brands,
+        'tags': tags,
+        'promotions': promotions,
+        'products': prod_page,
+        'product_count': prod_count,
+        'org': organization,
+        'store': store,
+        'local_css_library': settings.STORE_CSS_LIBRARY,
+        'local_js_library_header': settings.STORE_JS_LIBRARY_HEADER,
+        'local_js_library_body': settings.STORE_JS_LIBRARY_BODY,
     })
-
-
