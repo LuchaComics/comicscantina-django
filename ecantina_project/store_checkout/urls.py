665ba67f021fc8a4a12c19883d70f5fed8165821
Bartlomiej Mika <bmika@icloud.com>
2015-10-16 18:08:31 -0400

    Checkout page semi-finished #173.

--- ecantina_project/store_checkout/urls.py
+++ ecantina_project/store_checkout/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+from . import views
 
 
 urlpatterns = patterns('',
-
+    url(r'^cart$', views.cart_page),
+    url(r'^(\d+)/cart$', views.cart_page),
+    url(r'^(\d+)/(\d+)/cart$', views.cart_page),
 )
--- ecantina_project/store_checkout/views.py
+++ ecantina_project/store_checkout/views.py
@@ -1,3 +1,39 @@
+import json
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from django.conf import settings
+from api.models.ec.organization import Organization
+from api.models.ec.store import Store
+from api.models.ec.comic import Comic
+from api.models.ec.customer import Customer
+from api.models.ec.receipt import Receipt
 
-# Create your views here.
+
+def cart_page(request, org_id=0, store_id=0):
+    org_id = int(org_id)
+    store_id = int(store_id)
+    
+    # Fetch the Organization / Store.
+    org = Organization.objects.get_or_none(org_id)
+    store = Store.objects.get_or_none(store_id)
+    
+    # If user is logged in, fetch the Customer record or create one. Then
+    # fetch a Receipt record or create a new one.
+    customer = None
+    receipt = None
+    if request.user.is_authenticated():
+        customer = Customer.objects.get_or_create_for_user(request.user)
+        receipt = Receipt.objects.get_or_create_for_customer(customer)
+
+    # Display the view with all our model information.
+    return render(request, 'store_checkout/cart/view.html',{
+        'receipt': receipt,
+        'customer': customer,
+        'org': org,
+        'store': store,
+        'local_css_library' : settings.STORE_CSS_LIBRARY,
+        'local_js_library_header' : settings.STORE_JS_LIBRARY_HEADER,
+        'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
+        'page' : 'home',
+    })
