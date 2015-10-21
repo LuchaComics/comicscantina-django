b7f472afbb26e082c7b2c0f6fd9815c6e88307f7
Bartlomiej Mika <bmika@icloud.com>
2015-10-20 21:40:23 -0400

    BUGFIX: Fixed so the products get reserved.

--- ecantina_project/store_checkout/views.py
+++ ecantina_project/store_checkout/views.py
@@ -1,7 +1,6 @@
 import json
+from datetime import datetime
 from paypal.standard.forms import PayPalPaymentsForm
-from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received, payment_was_flagged
 from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
@@ -9,6 +8,7 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from ecantina_project import constants
 from api.models.ec.organization import Organization
 from api.models.ec.store import Store
 from api.models.ec.comic import Comic
@@ -179,10 +179,47 @@ def checkout_thank_you_page(request, param1_id=0, param2_id=0, param3_id=0):
     org = Organization.objects.get_or_none(int(org_id))
     store = Store.objects.get_or_none(int(store_id))
 
-    # Fetch OLD receipt
+    # Fetch OLD receipt and process it.
     old_receipt = Receipt.objects.get_or_none(receipt_id=receipt_id)
+
+    # STEP 1: Update Customer information.
+    billing_address = old_receipt.customer.billing_street_number
+    billing_address += ' ' + old_receipt.customer.billing_street_name
+    if old_receipt.customer.billing_unit_number:
+        billing_address = old_receipt.customer.billing_unit_number + '-' + billing_address
+            
+    shipping_address = old_receipt.customer.shipping_street_number
+    shipping_address += ' ' + old_receipt.customer.shipping_street_name
+    if old_receipt.customer.shipping_unit_number:
+        shipping_address = old_receipt.customer.shipping_unit_number + '-' + shipping_address
+    
+    old_receipt.email = old_receipt.customer.email
+    old_receipt.billing_name = old_receipt.customer.billing_name
+    old_receipt.billing_address = billing_address
+    old_receipt.billing_phone = old_receipt.customer.billing_phone
+    old_receipt.billing_city = old_receipt.customer.billing_city
+    old_receipt.billing_province = old_receipt.customer.billing_province
+    old_receipt.billing_country = old_receipt.customer.billing_country
+    old_receipt.billing_postal = old_receipt.customer.billing_postal
+    old_receipt.shipping_name = old_receipt.customer.shipping_name
+    old_receipt.shipping_address = shipping_address
+    old_receipt.shipping_phone = old_receipt.customer.shipping_phone
+    old_receipt.shipping_city = old_receipt.customer.shipping_city
+    old_receipt.shipping_province = old_receipt.customer.shipping_province
+    old_receipt.shipping_country = old_receipt.customer.shipping_country
+    old_receipt.shipping_postal = old_receipt.customer.shipping_postal
+        
+    # STEP 2: Finalize our receipt.
+    old_receipt.purchased = datetime.today()
     old_receipt.has_finished = True
+    old_receipt.status = constants.ONLINE_SALE_STATUS
+    old_receipt.payment_method = constants.PAYPAL_PAYMENT_METHOD
     old_receipt.save()
+        
+    # STEP 3: Inform our products that they are sold out.
+    for product in old_receipt.products.all():
+        product.is_sold = True
+        product.save()
 
     # Fetch Customer / Receipt
     # Note: Because our previous Receipt was set "has_finished" to true
