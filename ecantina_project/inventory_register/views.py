c296b2838895e96cdcec410926d680d5c579ba05
Bartlomiej Mika <bmika@icloud.com>
2015-10-25 13:06:08 -0400

    Added shipping rate calculator for Canada. #186

--- ecantina_project/inventory_register/views.py
+++ ecantina_project/inventory_register/views.py
@@ -15,10 +15,17 @@ from api.models.ec.customer import Customer
 from api.models.ec.employee import Employee
 from api.models.ec.store import Store
 from api.models.ec.section import Section
+from api.models.ec.orgshippingpreference import OrgShippingPreference
+from api.models.ec.orgshippingrate import OrgShippingRate
+from api.models.ec.store_shipping_preference import StoreShippingPreference
+from api.models.ec.store_shipping_rates import StoreShippingRate
 from inventory_base.forms.customerform import CustomerForm
 from inventory_setting.forms.userform import UserForm
 from inventory_setting.forms.organizationform import OrganizationForm
 from inventory_setting.forms.storeform import StoreForm
+from inventory_setting.forms.org_shipping_preference_form import OrgShippingPreferenceForm
+from inventory_setting.forms.org_shipping_rates_form import OrgShippingRateForm
+
 
 @login_required(login_url='/store/register/step1')
 def step1_page(request, org_id=0, store_id=0):
@@ -143,8 +150,60 @@ def step4_page(request, org_id=0, store_id=0):
     except Store.DoesNotExist:
         this_store = None
 
+    # Get our organization preferences.
+    try:
+        org_preference = OrgShippingPreference.objects.get(organization=this_org)
+    except OrgShippingPreference.DoesNotExist:
+        org_preference = OrgShippingPreference.objects.create(
+            organization_id = this_org.org_id,
+        )
+    
+    # If we don't have the shipping rates set then add them in now, else we
+    # just fetch the previously created rates.
+    ca_rate = None
+    us_rate = None
+    mx_rate = None
+    if len(org_preference.rates.all()) is 0:
+        # Create for Canda, United States and Mexico
+        canada = OrgShippingRate.objects.create(
+            organization_id = this_org.org_id,
+            country = 124,
+        )
+        org_preference.rates.add(canada)
+        united_states = OrgShippingRate.objects.create(
+            organization_id = this_org.org_id,
+            country = 840,
+        )
+        org_preference.rates.add(united_states)
+        mexico = OrgShippingRate.objects.create(
+            organization_id = this_org.org_id,
+            country = 484,
+        )
+        org_preference.rates.add(mexico)
+    
+        ca_rate = OrgShippingRate.objects.get(
+            organization_id=this_org.org_id,
+            country = 124,
+        )
+        us_rate = OrgShippingRate.objects.get(
+            organization_id=this_org.org_id,
+            country = 840,
+        )
+        mx_rate = OrgShippingRate.objects.get(
+            organization_id=this_org.org_id,
+            country = 484,
+        )
+    else:
+        ca_rate = OrgShippingRate.objects.get(organization_id=this_org.org_id,country=124)
+        us_rate = OrgShippingRate.objects.get(organization_id=this_org.org_id,country=840)
+        mx_rate = OrgShippingRate.objects.get(organization_id=this_org.org_id,country=484)
+
     # Display the view with all our model information.
     return render(request, 'inventory_register/step4/view.html',{
+        'org_form': OrgShippingPreferenceForm(instance=org_preference),
+        'ca_form': OrgShippingRateForm(instance=ca_rate),
+        'us_form': OrgShippingRateForm(instance=us_rate),
+        'mx_form': OrgShippingRateForm(instance=mx_rate),
         'form': StoreForm(instance=this_store),
         'employee': employee,
         'customer': customer,
