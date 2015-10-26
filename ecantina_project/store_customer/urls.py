2cd2d4fc47ca83e2dd004a1506311d712e61c9d8
Bartlomiej Mika <bmika@icloud.com>
2015-10-26 15:13:17 -0400

    Added placeholder code. #193

--- ecantina_project/store_customer/urls.py
+++ ecantina_project/store_customer/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from store_customer import views
 
 urlpatterns = patterns('',
-
+    url(r'my_account', views.my_account_page),
+    url(r'^(\d+)/my_account$', views.my_account_page),
+    url(r'^(\d+)/(\d+)/my_account$', views.my_account_page),
+                       
+    url(r'authentication', views.authentication_page),
+    url(r'^(\d+)/authentication$', views.authentication_page),
+    url(r'^(\d+)/(\d+)/authentication$', views.authentication_page),
 )
--- ecantina_project/store_customer/views.py
+++ ecantina_project/store_customer/views.py
@@ -1,3 +1,75 @@
+import json
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from django.conf import settings
+from api.models.ec.organization import Organization
+from api.models.ec.store import Store
+from api.models.ec.comic import Comic
+from api.models.ec.customer import Customer
+from api.models.ec.receipt import Receipt
+from api.models.ec.wishlist import Wishlist
 
-# Create your views here.
+
+def authentication_page(request, org_id=0, store_id=0):
+    org_id = int(org_id)
+    store_id = int(store_id)
+    
+    # Fetch the Organization / Store.
+    org = Organization.objects.get_or_none(org_id)
+    store = Store.objects.get_or_none(store_id)
+    
+    # If user is logged in, fetch the Customer record or create one. Then
+    # fetch a Receipt record or create a new one.
+    customer = None
+    receipt = None
+    wishlists = None
+    if request.user.is_authenticated():
+        customer = Customer.objects.get_or_create_for_user(request.user)
+        receipt = Receipt.objects.get_or_create_for_online_customer(customer)
+        wishlists = Wishlist.objects.filter_by_customer_id_or_none(customer.customer_id)
+    
+    # Display the view with all our model information.
+    return render(request, 'store_customer/authentication/view.html',{
+        'receipt': receipt,
+        'wishlists': wishlists,
+        'customer': customer,
+        'org': org,
+        'store': store,
+        'local_css_library' : settings.STORE_CSS_LIBRARY,
+        'local_js_library_header' : settings.STORE_JS_LIBRARY_HEADER,
+        'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
+        'page' : 'my_account',
+    })
+
+
+def my_account_page(request, org_id=0, store_id=0):
+    org_id = int(org_id)
+    store_id = int(store_id)
+    
+    # Fetch the Organization / Store.
+    org = Organization.objects.get_or_none(org_id)
+    store = Store.objects.get_or_none(store_id)
+    
+    # If user is logged in, fetch the Customer record or create one. Then
+    # fetch a Receipt record or create a new one.
+    customer = None
+    receipt = None
+    wishlists = None
+    if request.user.is_authenticated():
+        customer = Customer.objects.get_or_create_for_user(request.user)
+        receipt = Receipt.objects.get_or_create_for_online_customer(customer)
+        wishlists = Wishlist.objects.filter_by_customer_id_or_none(customer.customer_id)
+    
+    # Display the view with all our model information.
+    return render(request, 'store_customer/my_account/view.html',{
+        'receipt': receipt,
+        'wishlists': wishlists,
+        'customer': customer,
+        'org': org,
+        'store': store,
+        'local_css_library' : settings.STORE_CSS_LIBRARY,
+        'local_js_library_header' : settings.STORE_JS_LIBRARY_HEADER,
+        'local_js_library_body' : settings.STORE_JS_LIBRARY_BODY,
+        'page' : 'my_account',
+    })
