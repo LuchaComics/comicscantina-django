fd355e538ece4e57380f1d1d2a950fb79463bce0
Bartlomiej Mika <bmika@icloud.com>
2015-11-27 21:35:55 -0500

    BUGFIX; Added some fixes. #269

--- ecantina_project/store_about/views/about.py
+++ ecantina_project/store_about/views/about.py
@@ -6,10 +6,12 @@ from django.conf import settings
 from django.db.models import Q
 from api.models.ec.organization import Organization
 from api.models.ec.store import Store
+from api.models.ec.employee import Employee
+
 
 def about_page(request):
     employee = Employee.objects.get_for_user_id_or_none(request.user.id)
-    org = request.organization
+    organization = request.organization
     store = request.store
 
     # Redirect the user to a forbidden error if the store or organization
@@ -24,8 +26,8 @@ def about_page(request):
     # Fetch either all the stores within the organization or fetch the
     # individual store at the 'store_id' value.
     stores = None
-    if store_id > 0:
-        stores = Store.objects.filter(store_id=store_id)
+    if store:
+        stores = Store.objects.filter(store=store)
     else:
         stores = Store.objects.filter(organization=organization)
 
