3fab5c199532bf1bfe12be48461d0a3615bb4239
Bartlomiej Mika <bmika@icloud.com>
2016-01-05 10:28:53 -0500

    BUGFIX: Fixed so about can properly map to the view. #341

--- ecantina_project/store_about/urls.py
+++ ecantina_project/store_about/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from store_about.views import about
 
 urlpatterns = patterns('',
-    url(r'^about/$', about.about_page),
+    url(r'^about$', about.about_page),
 )
