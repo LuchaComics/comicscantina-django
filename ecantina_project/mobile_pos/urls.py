d507e1e8e59707c2bb76cd70125857568c8995ed
Bartlomiej Mika <bmika@icloud.com>
2016-01-11 18:13:02 -0500

    Unified all the views into a single view. #386

--- ecantina_project/mobile_pos/urls.py
+++ ecantina_project/mobile_pos/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import patterns, include, url
-from mobile_pos.views import pos_login
-from mobile_pos.views import pos_pick_store
-from mobile_pos.views import pos_dashboard
-from mobile_pos.views import pos_cart
-from mobile_pos.views import pos_scanner
-from mobile_pos.views import pos_checkout
-from mobile_pos.views import pos_remove_product
+from mobile_pos import views
 
 
 urlpatterns = patterns('',
-    url(r'^mobile/pos/login$', pos_login.login_page),
-    url(r'^mobile/pos/pick_store$', pos_pick_store.pick_store_page),
-    url(r'^mobile/pos/(\d+)/dashboard$', pos_dashboard.dashboard_page),
-    url(r'^mobile/pos/(\d+)/remove_product$', pos_remove_product.remove_product_page),
-    url(r'^mobile/pos/(\d+)/cart/(\d+)/$', pos_cart.cart_page),
-    url(r'^mobile/pos/(\d+)/cart/(\d+)/product_scanner$', pos_scanner.scanner_page),
-    url(r'^mobile/pos/(\d+)/cart/(\d+)/checkout$', pos_checkout.checkout_page),
+    url(r'^mobile/pos/login$', views.login_page),
+    url(r'^mobile/pos/pick_store$', views.pick_store_page),
+    url(r'^mobile/pos/(\d+)/dashboard$', views.dashboard_page),
+    url(r'^mobile/pos/(\d+)/remove_product$', views.remove_product_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/$', views.cart_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/product_scanner$', views.scanner_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/checkout$', views.checkout_page),
 )
