3d3081bd6293235e43d8cc7d837cb90e8fa99c64
Bartlomiej Mika <bmika@icloud.com>
2016-01-11 13:29:12 -0500

    Minor fixes. #386

--- ecantina_project/mobile_pos/urls.py
+++ ecantina_project/mobile_pos/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     url(r'^mobile/pos/login$', pos_login.login_page),
     url(r'^mobile/pos/(\d+)/dashboard$', pos_dashboard.dashboard_page),
     url(r'^mobile/pos/(\d+)/cart/(\d+)/$', pos_cart.cart_page),
-    url(r'^mobile/pos/(\d+)/cart/(\d+)/scanner$', pos_scanner.scanner_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/product_scanner$', pos_scanner.scanner_page),
     url(r'^mobile/pos/(\d+)/cart/(\d+)/checkout$', pos_checkout.checkout_page),
 )
