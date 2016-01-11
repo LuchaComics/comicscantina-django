b2c270e7a4d5291c8e9c9c6aad08734fab796387
Bartlomiej Mika <bmika@icloud.com>
2016-01-11 10:32:33 -0500

    Latest progress with developing the Cart.

--- ecantina_project/mobile_pos/urls.py
+++ ecantina_project/mobile_pos/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from mobile_pos.views import pos_login
 from mobile_pos.views import pos_dashboard
 from mobile_pos.views import pos_cart
-from mobile_pos.views import pos_search_customer
+from mobile_pos.views import pos_scanner
+from mobile_pos.views import pos_checkout
 
 
 urlpatterns = patterns('',
     url(r'^mobile/pos/login$', pos_login.login_page),
     url(r'^mobile/pos/(\d+)/dashboard$', pos_dashboard.dashboard_page),
     url(r'^mobile/pos/(\d+)/cart/(\d+)/$', pos_cart.cart_page),
-    url(r'^mobile/pos/(\d+)/cart/(\d+)/search_customer$', pos_search_customer.search_customer_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/scanner$', pos_scanner.scanner_page),
+    url(r'^mobile/pos/(\d+)/cart/(\d+)/checkout$', pos_checkout.checkout_page),
 )
