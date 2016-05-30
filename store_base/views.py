c3161674c61a6b4a5724eede4dc9fad0833c6867
Bartlomiej Mika <bart@mikasoftware.com>
2016-05-30 11:21:57 -0400

    BUGIFX: Removed code with using old secret_settings.py file.

--- store_base/views.py
+++ store_base/views.py
@@ -11,18 +11,18 @@ from api.models.ec.employee import Employee
 from api.models.ec.receipt import Receipt
 from api.models.ec.wishlist import Wishlist
 from api.models.ec.subdomain import SubDomain
-from ecantina_project.secret_settings import *
+from ecantina_project.settings import env_var
 
 
 def http_403_page(request):
     org_id = 0
     store_id = 0
     employee = Employee.objects.get_for_user_id_or_none(request.user.id)
-    
+
     # Fetch the Organization / Store.
     org = Organization.objects.get_or_none(org_id)
     store = Store.objects.get_or_none(store_id)
-    
+
     # If user is logged in, fetch the Customer record or create one. Then
     # fetch a Receipt record or create a new one.
     customer = None
@@ -32,7 +32,7 @@ def http_403_page(request):
         customer = Customer.objects.get_or_create_for_user_email(request.user.email)
         receipt = Receipt.objects.get_or_create_for_online_customer(customer)
         wishlists = Wishlist.objects.filter_by_customer_id_or_none(customer.customer_id)
-    
+
     # Display the view with all our model information.
     return render(request, 'store_base/403.html',{
         'receipt': receipt,
@@ -49,11 +49,11 @@ def http_404_page(request):
     org_id = 0
     store_id = 0
     employee = Employee.objects.get_for_user_id_or_none(request.user.id)
-    
+
     # Fetch the Organization / Store.
     org = Organization.objects.get_or_none(org_id)
     store = Store.objects.get_or_none(store_id)
-    
+
     # If user is logged in, fetch the Customer record or create one. Then
     # fetch a Receipt record or create a new one.
     customer = None
@@ -63,7 +63,7 @@ def http_404_page(request):
         customer = Customer.objects.get_or_create_for_user_email(request.user.email)
         receipt = Receipt.objects.get_or_create_for_online_customer(customer)
         wishlists = Wishlist.objects.filter_by_customer_id_or_none(customer.customer_id)
-    
+
     # Display the view with all our model information.
     return render(request, 'store_base/403.html',{
         'receipt': receipt,
@@ -86,8 +86,8 @@ def storefront_redirect(request, name=""):
         this_subdomain = SubDomain.objects.get(name=name)
     except SubDomain.DoesNotExist:
         return HttpResponseRedirect("/404")
-    
-    url = SECRET_HTTP_PROTOCOL+this_subdomain.name+"."+SECRET_DOMAIN
+
+    url = env_var('HTTP_PROTOCOL')+this_subdomain.name+"."+env_var('DOMAIN')
     return HttpResponseRedirect(url)
 
 
@@ -103,20 +103,20 @@ def org_subdomain_redirect(request, org_id=0):
         this_subdomain = this_subdomain[0]
     except SubDomain.DoesNotExist:
         this_subdomain = None
-    
-    url = SECRET_HTTP_PROTOCOL+this_subdomain.name+"."+SECRET_DOMAIN
+
+    url = env_var('HTTP_PROTOCOL')+this_subdomain.name+"."+env_var('DOMAIN')
     return HttpResponseRedirect(url)
 
 
 def store_subdomain_redirect(request, org_id=0, store_id=0):
     org_id = int(org_id)
     store_id = int(store_id)
-    
+
     # Find the subdomain associated with this store.
     try:
         this_subdomain = SubDomain.objects.get(store_id=store_id)
     except SubDomain.DoesNotExist:
         this_subdomain = None
-    
-    url = SECRET_HTTP_PROTOCOL+this_subdomain.name+"."+SECRET_DOMAIN
+
+    url = env_var('HTTP_PROTOCOL')+this_subdomain.name+"."+env_var('DOMAIN')
     return HttpResponseRedirect(url)
