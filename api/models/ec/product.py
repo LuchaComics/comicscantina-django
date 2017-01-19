481d14909ff221304d6296c2fc6af22c2420e3e7
Bartlomiej Mika <bart@mikasoftware.com>
2017-01-18 19:24:25 -0500

    BUGFIX: Deleted the delete override in products model class. #4

--- api/models/ec/product.py
+++ api/models/ec/product.py
@@ -25,11 +25,11 @@ class Product(models.Model):
         app_label = 'api'
         ordering = ('product_id','type')
         db_table = 'ec_products'
-    
+
     # All products have a unque 'product_id' number which is to be
     # printed on all products in-store or online.
     product_id = models.AutoField(primary_key=True)
-    
+
     # Every product needs to have a generic name that will show up
     # when customers lookup the product and staff look through the
     # inventory database; furthermore, a product type needs to be
@@ -41,31 +41,31 @@ class Product(models.Model):
         default=1,
         db_index=True,
     )
-    
+
     # description that will be displayed to the customer about this product.
     description = models.TextField(default='', blank=True)
-    
+
     # Date & timekeeping tracking variables.
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    
+
     # If this product has been purchased by a customer then "True" is set,
     # else "False". Stores are not to display products with 'True' condition.
     # It is important to note that when a product is purchased, the product
     # is not to be deleted from the table, but instead remain in the table
     # and just have the "is_sold" varible to be "True".
     is_sold = models.BooleanField(default=False, db_index=True)
-    
+
     # This variable controls whether we are allowed to display the product
     # in-store for customers to see or find in the catalog search. Products
     # with this variable set to false are not allowed to show up in search
     # results nor are customers allowed to see this product.
     is_listed = models.BooleanField(default=True, db_index=True)
-    
+
     # These two variables are used in the store.
     is_new = models.BooleanField(default=False, db_index=True)
     is_featured = models.BooleanField(default=False, db_index=True)
-    
+
     # The following variables are to save financial information.
     sub_price = models.DecimalField( # Note: Price before discount applied.
         max_digits=10,
@@ -119,28 +119,28 @@ class Product(models.Model):
         choices=constants.ISO_639_1_LANGUAGE_OPTIONS,
         default='EN',
     )
-                              
+
     # Every product has images.
     image = models.ForeignKey(ImageUpload, null=True, blank=True,)
     image_url = models.URLField(null=True, blank=True)
     images = models.ManyToManyField(ImageUpload, blank=True, related_name='product_images')
-    
+
     # Products need to belong to a specific organization and where it is located
     # in the organization (store/section).
     organization = models.ForeignKey(Organization, db_index=True)
     store = models.ForeignKey(Store, db_index=True)
     section = models.ForeignKey(Section, db_index=True)
-    
+
     # Every product has a list of tags they can belong to. Tags are used
     # to track th
     tags = models.ManyToManyField(Tag, blank=True, related_name='product_tags', db_index=True)
-    
+
     # Products should have a brand association with it.
     brand = models.ForeignKey(Brand, null=True, blank=True, db_index=True)
-    
+
     # Every product must belongs to a single cateogry.
     category = models.ForeignKey(Category, db_index=True)
-    
+
     # The QRCode image with the encoded "product_id" number in it and the boolean
     # to indicate whether the user printed the QR Code to a label or not.
     qrcode = models.ImageField(upload_to='qrcode', null=True, blank=True)
@@ -159,17 +159,17 @@ class Product(models.Model):
     def __str__(self):
         return str(self.name)
 
-    def delete(self, *args, **kwargs):
-        """
-            Overrided delete functionality to include deleting the local file
-            that we have stored on the system. Currently the deletion funciton
-            is missing this functionality as it's our responsibility to handle
-            the local files.
-        """
-        if self.qrcode:
-            if os.path.isfile(self.qrcode.path):
-                os.remove(self.qrcode.path)
-        super(Product, self).delete(*args, **kwargs) # Call the "real" delete() method
+    # def delete(self, *args, **kwargs):
+    #     """
+    #         Overrided delete functionality to include deleting the local file
+    #         that we have stored on the system. Currently the deletion funciton
+    #         is missing this functionality as it's our responsibility to handle
+    #         the local files.
+    #     """
+    #     if self.qrcode:
+    #         if os.path.isfile(self.qrcode.path):
+    #             os.remove(self.qrcode.path)
+    #     super(Product, self).delete(*args, **kwargs) # Call the "real" delete() method
 
     def save(self, *args, **kwargs):
         """
