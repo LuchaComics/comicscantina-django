44fc732e6d634844429d3e935748268632ea036e
bartmika <bart@mikasoftware.com>
2017-12-16 13:36:18 -0500

    Updated a few libraries and disabled SSL in S3 files

--- ecantina_project/settings.py
+++ ecantina_project/settings.py
@@ -250,7 +250,7 @@ AWS_STORAGE_BUCKET_NAME = env_var("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = env_var("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env_var("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
+AWS_S3_USE_SSL = False
 
 
 # Static files (CSS, JavaScript, Images) + Django-Storages
